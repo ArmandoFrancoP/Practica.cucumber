c88416dc849414c8e386d3c2102808c5cd960309
armando <armandofranco_92@hotmail.com>
2025-02-26 23:05:52 -0600

    primer commit

---
+++ src/test/java/Practica1/RunCucumberTest.java
@@ -0,0 +1,17 @@
+package Practica1;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectPackages("Practica1")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "Practica1")
+public class RunCucumberTest {
+}
